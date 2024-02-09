9b5624966b1bf85b83b0ddd96708eb74dc77f6aa
Sem4eZ <semen.fedorov3005@yandex.ru>
2024-02-09 13:54:44 +0500

    useLocation в Header

---
+++ src/pages/FullPizza.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FullPizza = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+      dicta aut molestias cumque dignissimos. Similique adipisci at est magni
+      debitis! Officiis itaque dolores commodi cumque culpa est, error velit
+      aut, vel ea, asperiores accusantium ipsum quam optio dignissimos
+      molestias. Consequatur assumenda impedit vitae quam quasi quidem ad
+      quaerat ipsum culpa.
+    </div>
+  );
+};
+
+export default FullPizza;
